3926fc2bd325981a70581021021e883c9929f2e1
Willian Jun Hasse <willianhasse@gmail.com>
2024-01-24 22:14:15 -0300

    username edit/show

--- svelte-demo/src/lib/server/admin.ts
+++ svelte-demo/src/lib/server/admin.ts
@@ -1,5 +1,5 @@
-//import { getAuth } from 'firebase-admin/auth';
-//import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
 import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private'
 import pkg from 'firebase-admin';
 
@@ -19,5 +19,5 @@ try {
   }
 }
 
-//export const adminDB = getFirestore();
-//export const adminAuth = getAuth();
+export const adminDB = getFirestore();
+export const adminAuth = getAuth();
--- svelte-demo/src/routes/login/username/+layout.svelte
+++ svelte-demo/src/routes/login/username/+layout.svelte
@@ -21,7 +21,7 @@
 </nav>
 
 <AnimatedRoute>
-	<main class="card w-8/10 bg-neutral text-neutral-content mx-auto">
+	<main class="card w-20/20 bg-neutral text-neutral-content mx-auto">
 		<div class="card-body items-center text-center">
 			<slot />
 		</div>
