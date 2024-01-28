8a9b23478bdfcf0fbd50e2ed20f77b1f86725d14
Willian Jun Hasse <willianhasse@gmail.com>
2024-01-28 11:38:13 -0300

    add datepicker examples

--- svelte-demo/tailwind.config.js
+++ svelte-demo/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}',
+            './node_modules/stwui/**/*.{svelte,js,ts,html}',
+            './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"),
+            require('stwui/plugin'),
+            require("tw-elements/dist/plugin.cjs")]
+  
 }
