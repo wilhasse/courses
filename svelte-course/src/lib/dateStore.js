e6d451bddf6f9fb8cb5565609dd937efaf1ac90b
Willian Jun Hasse <willianhasse@gmail.com>
2024-02-10 14:54:50 -0300

    joy of svelte: video 5 readable refactoring

--- svelte-course/src/lib/dateStore.js
+++ svelte-course/src/lib/dateStore.js
@@ -1,13 +1,9 @@
-import { readable } from'svelte/store';
+import { readable } from 'svelte/store';
 
-export default readable(new Date, callback => {
+export default readable(new Date, set => {
+	const interval = setInterval(() => {
+		set(new Date);
+	}, 1000);
 
-        callback(new Date);
-
-        const interval = setInterval(() => {
-            callback(new Date);
-        }, 1000);
-
-        return () => clearInterval(interval);
-
+	return () => clearInterval(interval);
 });
