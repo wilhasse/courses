23e8f32cfd3093e80f2779926834e715708d2a9a
Willian Jun Hasse <willianhasse@gmail.com>
2024-02-11 11:14:46 -0300

    vite using css modules

--- vite-demo/main.js
+++ vite-demo/main.js
@@ -3,8 +3,10 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import { debounce } from 'lodash-es'
+import styles from './example.module.css'
 
 console.log(debounce)
+console.log(styles)
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -14,7 +16,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1 class="${styles.title}">Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
