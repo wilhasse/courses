045e40c4a03103aece17a906a35f7496d103b33f
Willian Jun Hasse <willianhasse@gmail.com>
2024-02-16 23:16:50 -0300

    take selfie finished

--- vanilla-js-projects/selfie-cam/main.js
+++ vanilla-js-projects/selfie-cam/main.js
@@ -1,24 +1,11 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './src/counter.js'
+import { getVideo, drawVideo } from './src/camera.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const button = document.querySelector('button');
+const video = await getVideo();
+const canvas = document.querySelector('canvas');
 
-setupCounter(document.querySelector('#counter'))
+button.addEventListener('click', () => {
+ 
+  drawVideo(video, canvas);
+});
