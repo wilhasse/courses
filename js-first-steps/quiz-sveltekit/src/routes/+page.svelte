898445d9610893654f571db508d35b90c87db5ac
Willian Jun Hasse <willianhasse@gmail.com>
2024-02-24 09:10:52 -0300

    quiz sveltekit finished

--- js-first-steps/quiz-sveltekit/src/routes/+page.svelte
+++ js-first-steps/quiz-sveltekit/src/routes/+page.svelte
@@ -1,57 +1,86 @@
 <script>
 	import '../style.css';
-  import {facts} from '../data.js';
+	import { facts } from '../data.js';
 
-  let fact = facts.shift();
-  let explanation = "";
-  let correct = 0;
-  let completed = 0;
+	let fact = facts.shift();
+	let explanation = '';
+	let correct = 0;
+	let completed = 0;
+	let buttonTrue = '';
+	let buttonFalse = '';
+  let nextButton = 'Next Question';
+	let enableNext = false;
+  let enableAnswer = true;
 
-  function handleTrue() {
-    
-    // update counters
-    correct++;
-    completed++;
+	// @ts-ignore
+	function handleAnswer(event) {
+		const guess = event.target.value;
 
-    // display explanation
-    if (fact != undefined) {
-      explanation = fact.explanation;
-    }
+    // @ts-ignore
+    if (guess === fact.answer) {
+			// correct answer! Adding class style
+			if (event.target.name === 'true') {
+				buttonTrue = 'correct';
+			} else {
+				buttonFalse = 'correct';
+			}
+      correct += 1;
 
-    // next question
-    fact = facts.shift();
-  }
+		} else {
+			// wrong answer! Adding class style
+			if (event.target.name === 'true') {
+				buttonTrue = 'incorrect';
+			} else {
+				buttonFalse = 'incorrect';
+			}
+		}
 
-  function handleFalse() {
+		// update counter
+		completed++;
 
-    // update counter
-    completed++;
+		// display explanation
+		if (fact != undefined) {
+			explanation = fact.explanation;
+		}
 
-    // display explanation
-    if (fact != undefined) {
-     explanation = fact.explanation;
-    }
+		// last one ?
+		if (facts.length === 0) {
+			enableNext = false;
+      nextButton = "No more questions!";
+      enableAnswer = false;
+		} else {
+			enableNext = true;
+		}
+	}
 
-    // next question 
-    fact = facts.shift();
-  }
+	function nextQuestion() {
+		// clear text and classes
+		explanation = '';
+		buttonTrue = '';
+		buttonFalse = '';
+
+		// next question
+		fact = facts.shift();
+	}
 </script>
 
 <header>
-    <h1>Quiz.js</h1>
-    <p>Do you know JS? Find out!</p>
-    <div id="score">
-      Score: <span id="correct">{correct}</span> / <span id="completed">{completed}</span>
-    </div>
-  </header>
-  <main>
-    <div id="statement">{fact?.statement}</div>
-    <div id="options">
-      <button name="true" value="true" on:click={handleTrue}>true</button>
-      <button name="false" value="false" on:click={handleFalse}>false</button>
-    </div>
-    {#if explanation}
-    <div id="explanation">{explanation}</div>
-    {/if}
-    <button id="next-question" name="next-question">Next Question</button>
-  </main>
+	<h1>Quiz.js</h1>
+	<p>Do you know JS? Find out!</p>
+	<div id="score">
+		Score: <span id="correct">{correct}</span> / <span id="completed">{completed}</span>
+	</div>
+</header>
+<main>
+	<div id="statement">{fact?.statement}</div>
+	<div id="options">
+		<button disabled={!enableAnswer} class={buttonTrue} name="true" value="true" on:click={handleAnswer}>true</button>
+		<button disabled={!enableAnswer} class={buttonFalse} name="false" value="false" on:click={handleAnswer}>false</button>
+	</div>
+	{#if explanation}
+		<div id="explanation">{explanation}</div>
+	{/if}
+	<button disabled={!enableNext} id="next-question" name="next-question" on:click={nextQuestion}
+		>{nextButton}</button
+	>
+</main>
