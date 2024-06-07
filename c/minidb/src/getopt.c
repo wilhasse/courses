f6e02e491f6ab43aca4a644d321d9d7fb1cdef3d
wilhasse <willianhasse@gmail.com>
2024-06-07 13:13:59 -0300

    course minidb parsing header file

--- c/minidb/src/getopt.c
+++ c/minidb/src/getopt.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[]) {
 	bool newfile = false;
 	bool list = false;
 	int c;
+
 	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
 
 	while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
 		switch (c) {
@@ -54,14 +56,24 @@ int main(int argc, char *argv[]) {
 			printf("Unable to create database file\n");
 			return -1;
 		}
+		if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to create database header\n");
+			return -1;
+		}
 	} else {
 		dbfd = open_db_file(filepath);
 		if (dbfd == STATUS_ERROR) {
 			printf("Unable to open database file\n");
 			return -1;
 		}
+		if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to validate database header\n");
+			return -1;
+		}
 	}
 
 	printf("Newfile: %d\n" , newfile);
 	printf("Filepath: %s\n" , filepath);
+
+	output_file(dbfd,dbhdr);
 }
