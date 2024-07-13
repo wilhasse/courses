32cc7a644961a4ed5a347a8d6656afe496f03e8a
wilhasse <willianhasse@gmail.com>
2024-07-12 23:25:37 -0300

    go webserver - custom handler

--- go/webserver/main.go
+++ go/webserver/main.go
@@ -10,7 +10,9 @@ import (
 func main() {
 
 	m := http.NewServeMux()
+	m.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 	m.Handle("/", http.FileServer(http.Dir(".")))
+	m.HandleFunc("/healthz", handleOk)
 
 	const addr = ":8080"
 	srv := http.Server{
@@ -25,4 +27,11 @@ func main() {
 	fmt.Println("server started on ", addr)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func handleOk(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	const page = "OK"
+	w.Write([]byte(page))
 }
