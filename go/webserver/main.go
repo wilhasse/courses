268160d7a0a758ade71cb9ad4831b324a9d358a2
wilhasse <willianhasse@gmail.com>
2024-07-14 10:57:21 -0300

    count number of accesses , the method middlewareMetricsInc is incorrect, it is calling when adding to handle mutex

--- go/webserver/main.go
+++ go/webserver/main.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
 
+	apiCfg := apiConfig{}
+
 	m := http.NewServeMux()
-	m.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	m.Handle("/", http.FileServer(http.Dir(".")))
+	m.Handle("/app", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	m.Handle("/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("."))))
 	m.HandleFunc("/healthz", handleOk)
+	m.HandleFunc("/reset", apiCfg.reset)
+	m.HandleFunc("/metrics", apiCfg.metrics)
 
 	const addr = ":8080"
 	srv := http.Server{
@@ -27,6 +36,24 @@ func main() {
 	fmt.Println("server started on ", addr)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits += 1
+	return next
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	page := "Hits:" + strconv.Itoa(cfg.fileserverHits)
+	w.Write([]byte(page))
 }
 
 func handleOk(w http.ResponseWriter, r *http.Request) {
